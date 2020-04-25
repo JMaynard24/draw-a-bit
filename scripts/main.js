b456faf0a349e85b68865a84dcdd16f86acdf9fe
Vorondur <Vorondur@gmail.com>
2020-04-25 00:32:49 -0500

    Mostly working, slow, grab bug is annoying!

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
-const gridSize = 4;
+let gridSize = 128;
 const gridPixels = 512;
+let mouseDown = false;
+let tiles;
+let container = document.getElementById(`container`)
+let resetButton = document.getElementById(`reset-grid`)
+let sizeButton = document.getElementById(`change-size`)
 
 function createGrid(size)
 {
-    let container = document.getElementById(`container`)
     console.log(container);
     let start = 0;
     let tileAmount = size * size;
@@ -11,17 +15,80 @@ function createGrid(size)
     while (start < tileAmount)
     {
         let div = document.createElement('div');
-        div.id = 'tile';
+        div.classList = 'tile';
         div.style.height = tilePixelWidth + "px";
         div.style.width = tilePixelWidth + "px";
-        div.style.color = "#000000";
-        div.style.display = "inline-block";
+        div.style.backgroundColor = "#FFFFFF";
+        div.draggable = false;
         container.appendChild(div);
         start++;
     }
+    tiles = document.querySelectorAll('.tile')
+    tiles.forEach(tile => tileEvent(tile))
 }
 
 createGrid(gridSize);
+
+function changeColor(tile, newColor)
+{
+    tile.style.backgroundColor = newColor;
+}
+
+function changeGridSize()
+{
+    gridSize = prompt("How big would you like the grid to be? (??x??)");
+    while(container.firstChild)
+    {
+        container.removeChild(container.lastChild);
+    }
+    createGrid(gridSize);
+    
+}
+
+function resetGrid()
+{
+    tiles.forEach(tile => resetTile(tile));
+}
+
+function resetTile(tile)
+{
+    if (tile.style.backgroundColor != "#FFFFFF")
+    {
+        changeColor(tile, "#FFFFFF");
+    }
+}
+
+function mouseOff()
+{
+    mouseDown = false;
+    console.log(mouseDown);
+}
+
+function mouseOn()
+{
+    mouseDown = true;
+    console.log(mouseDown);
+}
+
+function tileEvent(tile)
+{
+    tile.addEventListener('mouseover', function(e)
+    {
+        if (mouseDown)
+        {
+            if (tile.backgroundColor != "#000000")
+            {
+                changeColor(tile, "#000000");
+            }
+        }
+    });
+}
+
+container.addEventListener('mousedown', mouseOn);
+container.addEventListener('mouseup', mouseOff);
+container.addEventListener('mouseleave', mouseOff);
+sizeButton.addEventListener('click', changeGridSize);
+resetButton.addEventListener('click', resetGrid);
 
 /*
  512 => 16 => 2x2
